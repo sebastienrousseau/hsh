9f2e386f98d2bfc67cef821c8f7a4940e6e88992
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-09-10 21:11:11 +0100

    doc(hsh): corrected label

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Copyright © 2023 Hash (HSH) library. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! This is the main entry point for the cmn application.
+//! This is the main entry point for the hsh application.
 fn main() {
     // Call the `run()` function from the `Hash (HSH)` module.
     if let Err(err) = hsh::run() {
